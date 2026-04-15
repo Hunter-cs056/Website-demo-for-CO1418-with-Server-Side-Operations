9e27b6dced1b7a1668434448b4a9f827f2a282dc
Maximos Philippou <MaximosGaming05@gmail.com>
2026-04-15 15:29:39 +0300

    Refactor navbar and back to top button functionality

--- myScript.js
+++ myScript.js
@@ -3,202 +3,34 @@
    ======================================== */
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
+if(hamburger && navMenu){
+	hamburger.addEventListener('click', () => {
+		navMenu.classList.toggle('active');
+	});
+}
 
 /* ========================================
-    PRODUCT PAGE
+   BACK TO TOP BUTTON
    ======================================== */
-//Products are now rendered server-side via PHP in products.PHP
-//The fitler is also handled in the same way using PHP
-
 //Lets add a back to top button functionality
 const backToTopBtn= document.getElementById('back-to-top');
 document.addEventListener('DOMContentLoaded', ()=>{
-if(backToTopBtn){
-//Initialy hide the button and show only on certain page height
-window.addEventListener('scroll', ()=>{
-	if(window.scrollY>=200){
-		backToTopBtn.style.display = 'block';
-	}
-	else{
-		backToTopBtn.style.display="none";
+	if(backToTopBtn){
+		//Initialy hide the button and show only on certain page height
+		window.addEventListener('scroll', ()=>{
+			if(window.scrollY>=200){
+				backToTopBtn.style.display = 'block';
+			}
+			else{
+				backToTopBtn.style.display="none";
+			}
+		});
+		//When the button is clicked we scroll to top
+		backToTopBtn.addEventListener('click', ()=>{
+			window.scrollTo({top:0, behavior: 'smooth'})
+		});
 	}
 });
-//When the button is clicked we scroll to top
-backToTopBtn.addEventListener('click', ()=>{
-	window.scrollTo({top:0, behavior: 'smooth'})
-});
-	}
-});
-/* ========================================
-    ITEM PAGE
-   ======================================== */
-//The item page is now fully server-side rendered using php in item.php
-//This is done using a database query based on the GET id parameter
-/* ========================================
-    ADD TO CART BUTTON'S FUNCTION
-   ======================================== */
-function addToCart(product){
-	//Product is always an object now(built server-side via json_encode in php)
-	const {id, name, price, stock, imgSrc, desc} = product;
-	//Load cart from our localstorage or start with an empty array
-	const cart = JSON.parse(localStorage.getItem('cart')) || [];
-	//Create a unique key to hold the product id
-	const key = `product-${id}`;
-	//Search our cart for the recently added product(return-1 if not found or the number otherwise)
-	const existingIndex = cart.findIndex(item => item.key === key);
-	
-	//Now check if it already exist and increase its quantity,otherwise push a new item
-	if(existingIndex !== -1){
-		cart[existingIndex].quantity += 1;
-	}
-	else {
-		const item = {key, id, name, price, stock, imgSrc, desc, quantity:1};
-		cart.push(item);
-	}
-	//Save the updated cart to localstorage
-	localStorage.setItem('cart',JSON.stringify(cart));
-	//Create alert box
-	alert(`${name} has been added to your Cart!`);
-}
-/* ========================================
-   CART PAGE(DISPLAY CART ITEMS)
-   ======================================== */
-//Access the html containers 
-const cartContainer = document.getElementById('cart-items');
-const emptyCartMsg= document.getElementById('empty-cart');
-const cartTotal = document.getElementById('cart-total')
 
-// CALCULATING AND DISPLAYING TOTAL
-//Calculate
-	function calculateTotal(cart){
-	return cart.reduce((sum, item) => {
-		//Here we remove the currency symbol and convert to number type
-		const price= parseFloat(item.price.replace(/[^\d.]/g, ''));
-		return sum + price * item.quantity;
-	}, 0);
-	}
-function renderCart(){
-	//Load cart from our localstorage or start with an empty array
-	const cart = JSON.parse(localStorage.getItem('cart')) || [];
-	//If its empty show message , clear items and exit the function
-	if(cart.length ==0){
-		cartContainer.innerHTML='';
-		emptyCartMsg.style.display='block';
-		cartTotal.innerHTML= '';
-		return;
-	}
-	//If not empty hide the empty Message
-	emptyCartMsg.style.display ='none';
-	cartContainer.innerHTML= '';
-	
-	//Loop through each item in the CART
-	cart.forEach(item => 
-	{	
-		//Create a row for the product and give it a class name
-		const row= document.createElement('div');
-		row.className= 'cart-row'
-		
-		//Load inside each row
-		row.innerHTML=`
-		<div class="cart-cell">
-			<img src="${item.imgSrc}" alt="${item.name}" class="cart-image">
-		</div>
-		<div class="cart-cell">
-			<span>${item.name}</span>
-			<br>
-			<a href="item.php?id=${item.id}" class="view-button">View More</a>
-		</div>
-		<div class="cart-cell">
-			<span>${item.price}</span>
-		</div>
-		<div class="cart-cell">
-			<button class="qty-btn" onclick="changeQuantity('${item.key}',-1)">-</button>
-			<input type="number" id="qty-input" class="qty-input" min="1" value="${item.quantity}" onchange="setQuantity('${item.key}', this.value)">
-			<button class="qty-btn" onclick="changeQuantity('${item.key}', 1)">+</button>
-		</div>
-		<div class="cart-cell">
-			<button class="remove-btn" onclick="removeItem('${item.key}')">Remove</button>
-		</div>
-		`;
-		cartContainer.appendChild(row);
-	});
 
-	//Display Total
-	const total= calculateTotal(cart); 
-	cartTotal.innerHTML = `<strong>Total: £${total.toFixed(2)} </strong>`;
-}
-//Create a discount code function that runs when the Apply button is clicked
-function applyCode(){
-	//Access the user input value and remove any extra spaces
-	const code =document.getElementById('code-input').value.trim();
-	//Load cart from our localstorage or start with an empty array
-	const cart = JSON.parse(localStorage.getItem('cart')) || [];
-	const total = calculateTotal(cart);
-	//Check if the user input matches in order to apply disount
-	if(code == "SAVE10"){
-		const discountedTotal =total*0.9;
-		cartTotal.innerHTML=`<strong>Discounted Total: £${discountedTotal.toFixed(2)}</strong>`;
-	}
-	//If the input does not match we display an alert
-	else{
-		alert("Invalid code ( reminder the code is: SAVE10 )");
-		cartTotal.innerHTML=`<strong>Total: £${total.toFixed(2)}</strong>`;
-	}
-}
-//Function that runs when the empty cart button is pressed
-function emptyCart(){
-	localStorage.removeItem('cart');
-	renderCart();
-}
-//Function to an item regardless of quantity
-function removeItem(key){
-	//Load cart from our localstorage or start with an empty array
-	let cart = JSON.parse(localStorage.getItem('cart')) || [];
-	cart= cart.filter(item=> item.key !== key); //remove the selected item 
-	localStorage.setItem('cart', JSON.stringify(cart)); // upload saved Cart
-	renderCart();
-}
-//Change quantity using the symbols function 
-function changeQuantity(key, delta){
-	//Load cart from our localstorage or start with an empty array
-	let cart = JSON.parse(localStorage.getItem('cart')) || [];
-	const index = cart.findIndex(item=> item.key == key);
-	//Identify which button was clicked and modify quantity
-	if (index !== -1){
-		cart[index].quantity += delta;
-	//If we drop the quantity to 0, remove the ITEM
-	if (cart[index].quantity <=0){
-		cart.splice(index, 1);
-	}
-	//Update Cart
-	localStorage.setItem('cart', JSON.stringify(cart));
-	renderCart();
-	}
-}
-//Change quantity by providing an input
-function setQuantity(key, newQty){
-	//Load cart from our localstorage or start  with an empty array
-	let cart = JSON.parse(localStorage.getItem('cart')) || [];
-	const index= cart.findIndex(item=> item.key == key);
-	
-
-	if(index !==-1){
-		let qty = parseInt(newQty, 10);
-		
-		// if the input is not valid reset to previous value
-		if(isNaN(qty) || qty < 1){
-			renderCart();
-			return
-		}
-		//Update our cart with valid quantity
-		cart[index].quantity = qty;
-		localStorage.setItem('cart' , JSON.stringify(cart));
-		renderCart();
-	}
-}
-//	Initial Render
-	renderCart();
 	
